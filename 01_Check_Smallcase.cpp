627913278a32fb11b364947071d72d5877132ad2
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-11-20 10:19:58 +0530

    Add files via upload

---
+++ 01_Check_Smallcase.cpp
@@ -0,0 +1,54 @@
+/* Accept Character from user and check whether its small case or not.
+
+Input- t
+Output- Yes its small case
+
+Input- T
+Output - No its not small case
+
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+#include<iostream>
+using namespace std;
+
+
+bool Check_SmallCase(char ch)
+{
+	 
+	if((ch>='a') && (ch<='z'))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+
+}
+
+
+int main()
+{
+    char ch='\0';
+    bool bRet=false;
+
+    cout<<"Enter a Character:"<<endl;
+    cin>>ch;
+
+    bRet=Check_SmallCase(ch);
+
+    if(bRet == true)
+    {
+      cout<<"Yes its small case"<<endl;
+    }
+    else
+    {
+      cout<<"No its not small case"<<endl;
+    }
+
+   
+	return 0;
+}
---
+++ 02_Check_Capitalcase.cpp
@@ -0,0 +1,53 @@
+/* Accept Character from user and check whether its Capital case or not.
+
+Input- N
+Output- Yes its Capital case
+
+Input- n
+Output - No its not Capital case
+
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include<iostream>
+using namespace std;
+
+
+bool Check_CapitalCase(char ch)
+{
+	 
+	if((ch>='A') && (ch<='Z'))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+
+}
+
+
+int main()
+{
+    char ch='\0';
+    bool bRet=false;
+
+    cout<<"Enter a Character:"<<endl;
+    cin>>ch;
+
+    bRet=Check_CapitalCase(ch);
+
+    if(bRet == true)
+    {
+      cout<<"Yes its Capital case"<<endl;
+    }
+    else
+    {
+      cout<<"No its not Capital case"<<endl;
+    }
+
+   
+	return 0;
+}
