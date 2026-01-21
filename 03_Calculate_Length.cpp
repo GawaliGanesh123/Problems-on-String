f0d9ff5dada4bc19252f5cb1e527e9e2487d9d6a
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-01-21 11:01:01 +0530

    Add files via upload

---
+++ 03_Calculate_Length.cpp
@@ -0,0 +1,31 @@
+// Write a program to Calculate the length of the string(Without using Built-in method).
+
+
+
+#include<iostream>
+using namespace std;
+
+void Strlenx(char *str)
+{
+  int iCnt=0;
+
+  while(*str !='\0')
+  {
+    iCnt++;
+    str++;
+  }
+
+  cout<<"Length of string is:"<<iCnt<<endl;
+}
+
+int main()
+{
+  char Arr[20];
+
+  cout<<"Enter a string:"<<endl;
+  cin.getline(Arr,20);
+
+  Strlenx(Arr);
+
+  return 0;
+}
