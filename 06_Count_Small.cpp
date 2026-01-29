1ea13833db4519a9c3c328b4c310fda3265b9a21
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-01-29 22:05:11 +0530

    Add files via upload

---
+++ 06_Count_Small.cpp
@@ -0,0 +1,47 @@
+/* Accept string from user and count the number of small letters from string.
+
+Input-  Ganesh Gawali.
+Output- 10
+
+Input- India Is My Country.
+Output-12
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include<iostream>
+using namespace std;
+
+int Count_Small(char *str)
+{
+	int iCnt=0;
+
+	while(*str !='\0')
+	{
+		if((*str>='a') && (*str<='z'))
+		{
+			iCnt++;
+		}
+
+		str++;
+	}
+
+	return iCnt;
+}
+
+int main()
+{
+    char Arr[20];
+    int  iRet=0;
+
+    cout<<"Enter a String:"<<endl;
+    cin.getline(Arr,20);
+
+    iRet=Count_Small(Arr);
+
+    cout<<"Count of Small letters : "<<iRet<<endl;
+
+
+    return 0;
+
+}
