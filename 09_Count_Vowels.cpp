a4907c1ab567ee407872ccc637ed48f9bf57b7d7
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-02-01 21:49:03 +0530

    Add files via upload

---
+++ 09_Count_Vowels.cpp
@@ -0,0 +1,53 @@
+/* Accept string from user and count the Vowels in a given string.
+
+Input-  Ganesh Gawali.
+Output- 5
+
+Input- India Is My Country.
+Output-6
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include<iostream>
+using namespace std;
+
+int Count_Vowels(char *str)
+{
+	int iCnt=0;
+
+	while(*str !='\0')
+	{
+		char ch = *str;
+
+        if(ch >= 'A' && ch <= 'Z')    // Convert uppercase to lowercase
+        {
+            ch = ch + 32;
+        }
+
+        if((ch=='a') || (ch=='e') || (ch=='i') || (ch=='o') || (ch=='u'))
+        {
+            iCnt++;
+        }
+        str++;
+    }
+
+    return iCnt;
+}
+
+int main()
+{
+    char Arr[20];
+    int  iRet=0;
+
+    cout<<"Enter a String:"<<endl;
+    cin.getline(Arr,20);
+
+    iRet=Count_Vowels(Arr);
+
+    cout<<"Count of Vowels : "<<iRet<<endl;
+
+
+    return 0;
+
+}
