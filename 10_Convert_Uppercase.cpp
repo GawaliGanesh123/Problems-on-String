3cacc0263c2c8f54422f97768c669bd213e74222
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-02-02 11:44:47 +0530

    Add files via upload

---
+++ 10_Convert_Uppercase.cpp
@@ -0,0 +1,42 @@
+/* Accept string from user and convert lower case letters into Uppercase.
+
+Input- Ganesh Gawali
+Output-GANESH GAWALI
+
+Input- amit
+Output- AMIT
+
+*/
+////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+#include<iostream>
+using namespace std;
+
+void Convert_To_Uppercase(char *str)
+{
+	while(*str !='\0')
+	{
+		if((*str>='a') && (*str<='z'))
+		{
+			*str=*str-32;
+		}
+
+		str++;
+	}
+}
+
+int main()
+{
+   char Arr[20];
+
+   cout<<"Enter a String:"<<endl;
+   cin.getline(Arr,20);
+
+   Convert_To_Uppercase(Arr);
+
+   cout<<"String After Conversion:"<<Arr<<endl;
+
+	return 0;
+}
