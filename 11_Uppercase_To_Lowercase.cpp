f4d43a8f48cfee94e9ce0ae5cce9d38caac76bd1
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-02-03 12:53:43 +0530

    Add files via upload

---
+++ 11_Uppercase_To_Lowercase.cpp
@@ -0,0 +1,43 @@
+/* Accept string from user and convert Uppercase letters into Lowercase.
+
+Input- Ganesh Gawali
+Output-ganesh gawali
+
+Input- NILESH
+Output- nilesh
+
+*/
+////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+#include<iostream>
+using namespace std;
+
+void Convert_To_Lowercase(char *str)
+{
+	while(*str !='\0')
+	{
+		if((*str>='A') && (*str<='Z'))
+		{
+			*str=*str+32;
+		}
+
+		str++;
+	}
+}
+
+
+int main()
+{
+   char Arr[20];
+
+   cout<<"Enter a String:"<<endl;
+   cin.getline(Arr,20);
+
+   Convert_To_Lowercase(Arr);
+
+   cout<<"String After Conversion:"<<Arr<<endl;
+
+	return 0;
+}
