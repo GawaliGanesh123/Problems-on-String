7335ce1b3e7c751aeeb6de401cde328e30be38f5
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-02-04 11:17:33 +0530

    Add files via upload

---
+++ 12_Toggle_case.cpp
@@ -0,0 +1,47 @@
+/* Accept string from user and convert Uppercase letters into Lowercase and Lowercase Letters into Uppercase(Toggle) .
+
+Input- Ganesh Gawali
+Output-gANESH gAWALI
+
+Input- NILESH
+Output- nilesh
+
+*/
+////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+#include<iostream>
+using namespace std;
+
+void Toggle_Case(char *str)
+{
+	while(*str !='\0')
+	{
+		if((*str>='A') && (*str<='Z'))
+		{
+			*str=*str+32;
+		}
+		else if((*str>='a') && (*str<='z'))
+		{
+			*str=*str-32;
+		}
+
+		str++;
+	}
+}
+
+
+int main()
+{
+   char Arr[20];
+
+   cout<<"Enter a String:"<<endl;
+   cin.getline(Arr,20);
+
+   Toggle_Case(Arr);
+
+   cout<<"String After Conversion:"<<Arr<<endl;
+
+	return 0;
+}
