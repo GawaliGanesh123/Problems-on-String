0dd7218a5f61a92dc3c392993c4ea3edf259b3e6
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-02-05 20:00:37 +0530

    Add files via upload

---
+++ 14_First_Occurence.cpp
@@ -0,0 +1,62 @@
+/* Accept string and one character from user and find first occurrence of a given character in a given string.
+
+Input- Ganesh
+     character:s
+
+Output-First Occurrence position is:4
+
+
+Input-Ravindra
+    character-z
+
+Output-Character not Found
+
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+int First_Occurrence(char *str, char ch)
+{
+	int i=0;
+
+	while(str[i] !='\0')
+	{
+		if(str[i] == ch)
+		{
+			return i;
+		}
+
+		i++;
+	}
+
+	return 0;
+}
+
+int main()
+{
+    char Arr[20];
+    char cValue='\0';
+    int iRet=0;
+
+    cout<<"Enter a String:"<<endl;
+    cin.getline(Arr,20);
+
+    cout<<"Enter a Character:"<<endl;
+    cin>>cValue;
+
+    iRet=First_Occurrence(Arr,cValue);
+
+    if(iRet != 0)
+    {
+    	cout<<"First Occurrence position is:"<<iRet<<endl;
+    }
+    else
+    {
+    	cout<<"Character not Found";
+    }
+
+
+	return 0;
+}
