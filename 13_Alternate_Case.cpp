7fb454f8ebc269d6aab9df68f1aaf3b1d386e2b9
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-02-05 08:48:31 +0530

    Add files via upload

---
+++ 13_Alternate_Case.cpp
@@ -0,0 +1,55 @@
+/* Accept string from user and convert it into Alternate Case.
+
+Input- Ganesh Gawali
+Output-GaNeSh gAwAlI
+
+Input- Aditya
+Output-AdItYa
+
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+#include<iostream>
+using namespace std;
+
+
+void Alternate_Case(char *str)
+{
+    int i = 0;
+
+    while(str[i] != '\0')
+    {
+        if(i % 2 == 0)                     // Evenindex=Capital
+        {
+            if((str[i] >= 'a') && (str[i] <= 'z'))
+            {
+                str[i] = str[i] - 32;
+            }
+        }
+        else             
+        {
+            if(str[i] >= 'A' && str[i] <= 'Z')
+            {
+                str[i] = str[i] + 32;
+            }
+        }
+
+        i++;
+    }
+}
+
+int main()
+{
+    char Arr[20];
+
+    cout<<"Enter a String:"<<endl;
+    cin.getline(Arr,20);
+
+    Alternate_Case(Arr);
+
+    cout<<"String After Conversion: "<<Arr<<endl;
+
+    return 0;
+}
