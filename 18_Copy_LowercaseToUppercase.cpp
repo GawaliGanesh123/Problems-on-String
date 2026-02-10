2b34be07914875575904259eacc345e67f915ef2
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-02-10 08:49:49 +0530

    Add files via upload

---
+++ 18_Copy_LowercaseToUppercase.cpp
@@ -0,0 +1,48 @@
+/*  Write a program  to accept one string (source string) from the user as input. 
+    Accept another empty string (destination string). 
+    Convert all Lowercase characters from the source string into uppercase characters and copy the converted result into the destination string. 
+    Finally, display the destination string.
+
+*/
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+#include<iostream>
+using namespace std;
+
+
+void Copy_Lower_To_Upper(char *src, char *dest)
+{
+	while(*src !='\0')
+	{
+		if((*src>='a') &&(*src<='z'))
+		{
+		  *dest=*src-32;	
+		}
+		else
+		{
+			*dest=*src;
+		}
+
+		src++;
+		dest++;
+	}
+	*dest='\0';
+}
+
+
+int main()
+{
+  char Arr[20];
+  char Brr[20];
+
+  cout<<"Enter a string:"<<endl;
+  cin.getline(Arr,20);
+
+  Copy_Lower_To_Upper(Arr,Brr);
+
+  cout<<"String After Copy:"<<Brr<<endl;
+
+	return 0;
+}
