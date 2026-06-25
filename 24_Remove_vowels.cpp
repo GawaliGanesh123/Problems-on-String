bc7149f13c8e5c5d3c0ba21bf94d7b4ae87a70d5
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-06-25 16:26:16 +0530

    Add files via upload

---
+++ 24_Remove_vowels.cpp
@@ -0,0 +1,60 @@
+/* Accept string from user and remove the Vowels from a given string.
+
+Input-  Ganesh Gawali.
+Output- Gnsh Gwl.
+
+Input- India Is My Country.
+Output- nd s My Cntry.
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+void Remove_Vowels(char *str)
+{
+    int i = 0, j = 0;
+    char temp[20];
+
+    while(str[i] != '\0')
+    {
+        char ch = str[i];
+
+        if(ch >= 'A' && ch <= 'Z')    // Convert uppercase to lowercase
+        {
+            ch = ch + 32;
+        }
+
+        if(!((ch=='a') || (ch=='e') || (ch=='i') || (ch=='o') || (ch=='u')))
+        {
+            temp[j] = str[i];   // store original character
+            j++;
+        }
+        i++;
+    }
+
+    temp[j] = '\0';
+
+    // Copy back to original string
+    i = 0;
+    while(temp[i] != '\0')
+    {
+        str[i] = temp[i];
+        i++;
+    }
+    str[i] = '\0';
+}
+
+int main()
+{
+    char Arr[20];
+
+    cout<<"Enter a String:"<<endl;
+    cin.getline(Arr,20);
+
+    Remove_Vowels(Arr);
+
+    cout<<"String after removing vowels : "<<Arr<<endl;
+
+    return 0;
+}
