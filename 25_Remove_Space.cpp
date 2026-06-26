7d4f8071a6e589173904527937bd2b5485947d92
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-06-26 11:57:34 +0530

    Add files via upload

---
+++ 25_Remove_Space.cpp
@@ -0,0 +1,43 @@
+/* Accept string from user and remove the Spaces from a given string.
+
+Input-  Ganesh Gawali.
+Output- GaneshGawali.
+
+Input- India Is My Country.
+Output- IndiaIsMyCountry.
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+void Remove_Spaces(char *str)
+{
+    int i = 0, j = 0;
+
+    while(str[i] != '\0')
+    {
+        if(str[i] != ' ')   // skip spaces
+        {
+            str[j] = str[i];
+            j++;
+        }
+        i++;
+    }
+
+    str[j] = '\0';   // end string
+}
+
+int main()
+{
+    char Arr[20];
+
+    cout<<"Enter a String:"<<endl;
+    cin.getline(Arr,20);
+
+    Remove_Spaces(Arr);
+
+    cout<<"String after removing spaces : "<<Arr<<endl;
+
+    return 0;
+}
