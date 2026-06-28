c2dfe8b7c4915fc49d91875e4996c3c5259a96f9
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-06-28 16:14:44 +0530

    Add files via upload

---
+++ 26_Remove_Consonants.cpp
@@ -0,0 +1,53 @@
+/* Accept string from user and remove the Consonants from a given string.
+
+Input-  Ganesh Gawali.
+Output- aea aai.
+
+Input- India Is My Country.
+Output- Iia I ouy.
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+void Remove_Consonants(char *str)
+{
+    int i = 0, j = 0;
+
+    while(str[i] != '\0')
+    {
+        char ch = str[i];
+
+        // Convert uppercase to lowercase for checking
+        if(ch >= 'A' && ch <= 'Z')
+        {
+            ch = ch + 32;
+        }
+
+        // Keep only vowels and spaces
+        if((ch=='a') || (ch=='e') || (ch=='i') || (ch=='o') || (ch=='u') || (str[i]==' '))
+        {
+            str[j] = str[i];
+            j++;
+        }
+
+        i++;
+    }
+
+    str[j] = '\0';
+}
+
+int main()
+{
+    char Arr[20];
+
+    cout<<"Enter a String:"<<endl;
+    cin.getline(Arr,20);
+
+    Remove_Consonants(Arr);
+
+    cout<<"String after removing consonants : "<<Arr<<endl;
+
+    return 0;
+}
