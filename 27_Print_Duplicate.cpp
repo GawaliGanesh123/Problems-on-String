82e6f0963a39f7781822b11f9623977f188036ac
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-06-29 10:16:14 +0530

    Add files via upload

---
+++ 27_Print_Duplicate.cpp
@@ -0,0 +1,66 @@
+/* Accept string from user and print the duplicate characters from a given string.
+
+Input-  programming
+Output- r g m
+
+Input-  Ganesh Gawali
+Output- a
+
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include<iostream>
+using namespace std;
+
+void Print_Duplicate_Characters(char *str)
+{
+    char *ptr = str;
+
+    while(*ptr != '\0')
+    {
+        int iCnt = 0;
+        char *temp = str;
+
+        while(*temp != '\0')
+        {
+            if(*ptr == *temp)
+            {
+                iCnt++;
+            }
+            temp++;
+        }
+
+        if(iCnt > 1)
+        {
+            cout << *ptr << " ";
+
+            // Mark it as a  all occurrences as visited
+            temp = str;
+            while(*temp != '\0')
+            {
+                if(*temp == *ptr)
+                {
+                    *temp = '*';
+                }
+                temp++;
+            }
+        }
+
+        ptr++;
+    }
+}
+
+int main()
+{
+    char Arr[50];
+
+    cout<<"Enter a String:"<<endl;
+    cin.getline(Arr,50);
+
+    Print_Duplicate_Characters(Arr);
+
+    cout<<endl;
+
+    return 0;
+}
