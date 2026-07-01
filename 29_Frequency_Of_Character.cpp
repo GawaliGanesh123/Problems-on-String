85f7cea484336e850e0eedee96aecaf741953db7
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-07-01 10:03:54 +0530

    Add files via upload

---
+++ 29_Frequency_Of_Character.cpp
@@ -0,0 +1,61 @@
+/* Accept string from user and display frequency of each character.
+
+Input  : apple
+Output :
+a = 1
+p = 2
+l = 1
+e = 1
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+void Char_Frequency(char *str)
+{
+    int i = 0;
+
+    while(str[i] != '\0')
+    {
+        char ch = str[i];
+        int count = 0;
+        bool printed = false;
+
+        // Check if character was already printed
+        for(int k = 0; k < i; k++)
+        {
+            if(str[k] == ch)
+            {
+                printed = true;
+                break;
+            }
+        }
+
+        if(!printed)
+        {
+            // Count occurrences
+            for(int j = 0; str[j] != '\0'; j++)
+            {
+                if(str[j] == ch)
+                    count++;
+            }
+
+            cout << ch << " = " << count << endl;
+        }
+
+        i++;
+    }
+}
+
+int main()
+{
+    char Arr[50];
+
+    cout << "Enter a String:" << endl;
+    cin.getline(Arr, 50);
+
+    Char_Frequency(Arr);
+
+    return 0;
+}
