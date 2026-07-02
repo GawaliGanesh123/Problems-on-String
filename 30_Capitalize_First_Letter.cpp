dfc209d9b30a93e9ff53de7ec935fda17a75011b
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-07-02 10:25:12 +0530

    Add files via upload

---
+++ 30_Capitalize_First_Letter.cpp
@@ -0,0 +1,51 @@
+/* Accept string from user and capitalize the first letter of each word.
+
+Input-  i love coding
+Output- I Love Coding
+
+Input-  india is my country
+Output- India Is My Country
+
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+void Capitalize_First_Letter(char *str)
+{
+    int i = 0;
+
+    // Capitalize first character if it is lowercase
+    if(str[0] >= 'a' && str[0] <= 'z')
+    {
+        str[0] = str[0] - 32;
+    }
+
+    while(str[i] != '\0')
+    {
+        if(str[i] == ' ')
+        {
+            if(str[i+1] >= 'a' && str[i+1] <= 'z')
+            {
+                str[i+1] = str[i+1] - 32;
+            }
+        }
+
+        i++;
+    }
+}
+
+int main()
+{
+    char Arr[50];
+
+    cout<<"Enter a String:"<<endl;
+    cin.getline(Arr,50);
+
+    Capitalize_First_Letter(Arr);
+
+    cout<<"String after capitalization : "<<Arr<<endl;
+
+    return 0;
+}
