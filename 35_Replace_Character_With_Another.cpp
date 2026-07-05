b2e7d04ca6a0cce95d189a9f8b1ad96ef720b502
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-07-05 18:03:13 +0530

    Add files via upload

---
+++ 35_Replace_Character_With_Another.cpp
@@ -0,0 +1,54 @@
+/* Accept string from user and replace a given character with another character.
+
+Input-
+String  : banana
+Replace 'a' with 'o'
+
+Output- bonono
+
+Input-
+String  : Ganesh
+Replace 'a' with 'u'
+
+Output- Gunesh
+
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+void Replace_Character(char *str, char chOld, char chNew)
+{
+    while(*str != '\0')
+    {
+        if(*str == chOld)
+        {
+            *str = chNew;
+        }
+
+        str++;
+    }
+}
+
+int main()
+{
+    char Arr[50];
+    char chOld = '\0';
+    char chNew = '\0';
+
+    cout<<"Enter a String:"<<endl;
+    cin.getline(Arr,50);
+
+    cout<<"Enter character to replace:"<<endl;
+    cin>>chOld;
+
+    cout<<"Enter new character:"<<endl;
+    cin>>chNew;
+
+    Replace_Character(Arr, chOld, chNew);
+
+    cout<<"String after replacement : "<<Arr<<endl;
+
+    return 0;
+}
