49a59d751ed3e0ad993e2df354e5d65650c7b39f
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-07-09 23:16:16 +0530

    Add files via upload

---
+++ 41_Check_Digits.cpp
@@ -0,0 +1,50 @@
+/* Accept string from user and check whether it contains only digits.
+
+Input-  12345
+Output- String contains only digits
+
+Input-  12A45
+Output- String contains non-digit characters
+
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+bool Check_Digits(char *str)
+{
+    while(*str != '\0')
+    {
+        if(*str < '0' || *str > '9')
+        {
+            return false;
+        }
+
+        str++;
+    }
+
+    return true;
+}
+
+int main()
+{
+    char Arr[50];
+    bool bRet = false;
+
+    cout<<"Enter a String:"<<endl;
+    cin.getline(Arr,50);
+
+    bRet = Check_Digits(Arr);
+
+    if(bRet == true)
+    {
+        cout<<"String contains only digits"<<endl;
+    }
+    else
+    {
+        cout<<"String contains non-digit characters"<<endl;
+    }
+
+    return 0;
+}
