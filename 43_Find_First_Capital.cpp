a9ec197c9fc4bbb1077a5cfcb9a74760c4d76e63
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-07-13 22:49:40 +0530

    Add files via upload

---
+++ 43_Find_First_Capital.cpp
@@ -0,0 +1,50 @@
+/* Accept string from user and find the first capital letter.
+
+Input-  ganEsh
+Output- E
+
+Input-  india Is My Country
+Output- I
+
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+char First_Capital_Letter(char *str)
+{
+    while(*str != '\0')
+    {
+        if(*str >= 'A' && *str <= 'Z')
+        {
+            return *str;
+        }
+
+        str++;
+    }
+
+    return '\0';
+}
+
+int main()
+{
+    char Arr[50];
+    char chRet = '\0';
+
+    cout<<"Enter a String:"<<endl;
+    cin.getline(Arr,50);
+
+    chRet = First_Capital_Letter(Arr);
+
+    if(chRet != '\0')
+    {
+        cout<<"First Capital Letter : "<<chRet<<endl;
+    }
+    else
+    {
+        cout<<"No Capital Letter Found"<<endl;
+    }
+
+    return 0;
+}
