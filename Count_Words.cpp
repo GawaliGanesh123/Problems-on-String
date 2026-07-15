946c3d129a5c907e37b4a2582e149de2dcc831c7
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-07-15 12:45:20 +0530

    Add files via upload

---
+++ Count_Words.cpp
@@ -0,0 +1,44 @@
+/* Accept string from user and count the words in a given string.
+
+Input-  Ganesh Gawali.
+Output- 2
+
+Input- India Is My Country.
+Output- 4
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+int Count_Words(char *str)
+{
+    int i = 0, iCnt = 0;
+
+    while(str[i] != '\0')
+    {
+        // Check start of a word
+        if((str[i] != ' ') && (i == 0 || str[i-1] == ' '))
+        {
+            iCnt++;
+        }
+        i++;
+    }
+
+    return iCnt;
+}
+
+int main()
+{
+    char Arr[50];
+    int iRet = 0;
+
+    cout<<"Enter a String:"<<endl;
+    cin.getline(Arr,50);
+
+    iRet = Count_Words(Arr);
+
+    cout<<"Count of Words : "<<iRet<<endl;
+
+    return 0;
+}
